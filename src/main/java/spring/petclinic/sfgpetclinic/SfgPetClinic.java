ff653b9a8cce7ddb1346d7a075701cd0fb2ed431
coderiddles <godspoweropara37@gmail.com>
2020-02-10 23:25:48 +0100

    Initia Project Setup

---
+++ src/main/java/spring/petclinic/sfgpetclinic/SfgPetClinic.java
@@ -0,0 +1,13 @@
+package spring.petclinic.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinic.class, args);
+	}
+
+}
